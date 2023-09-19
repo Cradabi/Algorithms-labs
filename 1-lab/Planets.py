42715c83a41dd1643cf7b60896d298fda95a3f82
Nyehx <144653367+Nyehx@users.noreply.github.com>
2023-09-19 16:36:11 +0300

    Add files via upload
    amongus

---
+++ 1-lab/Planets.py
@@ -0,0 +1,44 @@
+''' На какой-то планете находится x городов. Матрица x*x с 0 и 1, где 1 - там дорога.
+Если нет дорог, то города в других странах. Посчитать, сколько стран на планете.
+'''
+
+import random
+
+scale = 20
+
+rng = [0] * 20 + [1]
+matrix = [[random.choice(rng) for x in range(scale)] for x in range(scale)]
+
+borders = {x: [x] for x in range(scale)}
+
+
+for i in range(scale):
+    matrix[i][i] = 1
+    
+for x in range(scale):
+    for y in range(scale):
+        if matrix[x][y] == 1 and x!= y:
+            borders[x].append(y)
+
+
+
+countries = {x: set() for x in range(scale)}
+
+for y in range(scale):
+    z = borders[y]
+    for i in z:
+        countries[y].add(i)
+
+for x in range(scale):
+    for y in range(scale):
+        if x in countries and y in countries and x!=y:
+            k = countries[x]
+            l = countries[y]
+            if len(k.intersection(l)) > 0:
+                countries[x].update(l)
+                del countries[y]
+
+bob = len(countries)
+print(f'Количество стран на планете - {bob}')
+ 
+            
