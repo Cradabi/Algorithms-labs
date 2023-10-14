2a19e9e968ffd70ea603d482d0260d58bfee7f0e
Daniel <75375225+Cradabi@users.noreply.github.com>
2023-10-14 14:57:57 +0300

    Update task_4.py

--- 3-lab/task_4.py
+++ 3-lab/task_4.py
@@ -1,5 +1,5 @@
 def insertion_sort(list_n):
-    for i in range(len(list_n)):
+    for i in range(1, len(list_n)):
         j = i - 1
         num_n = list_n[i]
         while list_n[j] > num_n and j >= 0:
