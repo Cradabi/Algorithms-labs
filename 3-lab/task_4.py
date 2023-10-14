c24464f4af873c23c56aff3de61771d6b8df95fb
Daniel <75375225+Cradabi@users.noreply.github.com>
2023-10-14 14:55:47 +0300

    task_4

--- 3-lab/task_4.py
+++ 3-lab/task_4.py
@@ -1,5 +1,5 @@
 def insertion_sort(list_n):
-    for i in range(1, len(list_n)):
+    for i in range(len(list_n)):
         j = i - 1
         num_n = list_n[i]
         while list_n[j] > num_n and j >= 0:
@@ -9,4 +9,5 @@ def insertion_sort(list_n):
     return list_n
 
 
-print(insertion_sort([5, 3, 12, 1, 16, 8]))
+a = result = list(map(int, input("Введите числа: ").split()))
+print(insertion_sort(a))
