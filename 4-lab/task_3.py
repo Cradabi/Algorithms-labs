4624428186dea78c34dd515e4ce93ff8abcc3c91
Daniel <danya.butskiy@gmail.com>
2023-10-30 00:25:31 +0300

    Update task_3.py

--- 4-lab/task_3.py
+++ 4-lab/task_3.py
@@ -1,32 +1,56 @@
 import random
+import sys
+
+
+
+
+sys.setrecursionlimit(5000000)
 
 
 def go(index_now, all_list, visit_list, way_list):
-    if index_now[1] == 14:
-        return "Путь существует"
+    visit_list.append(index_now)
+    if len(way_list) > 0:
+        if way_list[-1] != index_now:
+            way_list.append(index_now)
+    else:
+        way_list.append(index_now)
+    if index_now[1] == 5:
+        print(way_list)
+        return way_list
     if (index_now[0] - 1) >= 0 and (index_now[0] - 1, index_now[1]) not in visit_list and all_list[
-        index_now[0] - 1, index_now[1]] == 0:
-        pass
+        index_now[0] - 1][index_now[1]] == 0:
+        go((index_now[0] - 1, index_now[1]), all_list, visit_list, way_list)
     elif (index_now[1] - 1) >= 0 and (index_now[0], index_now[1] - 1) not in visit_list and all_list[
-        index_now[0], index_now[1] - 1] == 0:
-        pass
-    elif (index_now[0] + 1) <= 14 and (index_now[0], index_now[1] - 1) not in visit_list and all_list[
-        index_now[0] + 1, index_now[1]] == 0:
-        pass
-    elif (index_now[1] + 1) <= 14 and (index_now[0], index_now[1] - 1) not in visit_list and all_list[
-        index_now[0], index_now[1] + 1] == 0:
-        pass
+        index_now[0]][index_now[1] - 1] == 0:
+        go((index_now[0], index_now[1] - 1), all_list, visit_list, way_list)
+    elif (index_now[0] + 1) <= 5 and (index_now[0] + 1, index_now[1]) not in visit_list and all_list[
+        index_now[0] + 1][index_now[1]] == 0:
+        go((index_now[0] + 1, index_now[1]), all_list, visit_list, way_list)
+    elif (index_now[1] + 1) <= 5 and (index_now[0], index_now[1] + 1) not in visit_list and all_list[
+        index_now[0]][index_now[1] + 1] == 0:
+        go((index_now[0], index_now[1] + 1), all_list, visit_list, way_list)
+    else:
+        way_list.pop(-1)
+        if len(way_list) == 0:
+            return 0
+        else:
+            go(way_list[-1], all_list, visit_list, way_list)
+
+
 
 
 visit_list = []
 x = []
 all_list = []
-for i in range(15):
-    for w in range(15):
+for i in range(6):
+    for w in range(6):
         x.append(random.randint(0, 1))
+    print(x)
     all_list.append(x)
     x = []
-print(all_list)
 
-bad_list = []
-lf = (21, 23)
+res = []
+for i in range(6):
+    if all_list[i][0] == 0:
+        x.append(go((i, 0), all_list, [], []))
+print(x)
