88ad225457e8d27faf2efc54e411634e55023557
Daniel <75375225+Cradabi@users.noreply.github.com>
2024-05-16 23:31:53 +0300

    Update task-11.py

--- 9-lab/task-11.py
+++ 9-lab/task-11.py
@@ -1,47 +1,102 @@
-import heapq
+import sys
 
 
-def shortest_path_with_limit(graph, start, end, extra_paths):
-    queue = [(0, start, extra_paths)]
-    visited = set()
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
 
-    while queue:
-        cost, current, extra = heapq.heappop(queue)
+    def construct_graph(self, nodes, init_graph):
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
 
-        if current == end:
-            return cost
+        graph.update(init_graph)
 
-        if (current, extra) in visited:
-            continue
-        visited.add((current, extra))
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
 
-        for neighbor, weight, limit in graph[current]:
-            new_cost = cost + weight
-            new_extra = extra
-            if limit > 0:
-                new_extra = extra - 1
+        return graph
 
-            if new_extra >= 0:
-                heapq.heappush(queue, (new_cost, neighbor, new_extra))
+    def get_nodes(self):
+        return self.nodes
 
-    return None
+    def get_outgoing_edges(self, node):
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        return self.graph[node1][node2]
 
 
-# Пример графа
-graph = {
-    'A': [('B', 1, 0), ('C', 5, 1)],
-    'B': [('C', 2, 0)],
-    'C': [('D', 3, 0)],
-    'D': [('E', 4, 0)],
-    'E': []
-}
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+    shortest_path = {}
+    previous_nodes = {}
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    shortest_path[start_node] = 0
 
-start = 'A'
-end = 'E'
-extra_paths = 1
+    while unvisited_nodes:
+        current_min_node = None
+        for node in unvisited_nodes:  # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
 
-result = shortest_path_with_limit(graph, start, end, extra_paths)
-if result is not None:
-    print(f"Самый короткий путь между вершинами {start} и {end} равен {result}")
-else:
-    print("Путь не найден")
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+
+        unvisited_nodes.remove(current_min_node)
+
+    return previous_nodes, shortest_path
+
+
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+
+    # Добавить начальный узел вручную
+    path.append(start_node)
+
+    print("Длина кратчайшего пути равна - {}.".format(shortest_path[target_node]))
+    print("Сам путь: " + " - ".join(reversed(path)))
+
+
+nodes = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+init_graph = {}
+for node in nodes:
+    init_graph[node] = {}
+
+init_graph["1"]["2"] = 5
+init_graph["1"]["4"] = 4
+init_graph["2"]["6"] = 1
+init_graph["2"]["3"] = 3
+init_graph["3"]["7"] = 5
+init_graph["3"]["8"] = 4
+init_graph["8"]["7"] = 1
+init_graph["5"]["6"] = 2
+init_graph["5"]["8"] = 2
+
+graph = Graph(nodes, init_graph)
+
+previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node="1")
+
+print_result(previous_nodes, shortest_path, start_node="1", target_node="7")
